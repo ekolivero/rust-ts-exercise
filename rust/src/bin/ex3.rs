2da4457c1248cca9e94335d8a29300c117c58f91
Erik Olivero <erik.olivero18@gmail.com>
2022-09-04 11:16:39 +0200

    Add rust example 3

---
+++ rust/src/bin/ex3.rs
@@ -0,0 +1,28 @@
+fn get_input() -> &'static str {
+  return "..##.......
+#...#...#..
+.#....#..#.
+..#.#...#.#
+.#...##..#.
+..#.##.....
+.#.#.#....#
+.#........#
+#.##...#...
+#...##....#
+.#..#...#.#"
+}
+
+fn main() {
+    let matching = get_input()
+        .lines()
+        .enumerate()
+        .flat_map(|(idx, line)| {
+            return line.chars()
+                .nth(idx * 3 % line.len())
+        })
+        .filter(|&x| x == '#')
+        .count();
+
+    println!("The total number is {}", matching);
+
+}
